b07201fd578b0532dcf7e731559258122264bbad
Marvin Rensing <9006725+MetroMarv@users.noreply.github.com>
2025-10-02 14:56:21 +0200

    Solve Assignment 5.1

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,5 @@
 import { Routes } from '@angular/router';
 import { HomePage } from './home/home-page/home-page';
-import { ProductsPage } from './products/products-page/products-page';
 
 export const routes: Routes = [{
         path: '',
