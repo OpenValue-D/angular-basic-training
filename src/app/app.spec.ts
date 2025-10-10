b4e936e99048e317ff3e909e576837dd3b69570c
Marvin Rensing <9006725+MetroMarv@users.noreply.github.com>
2025-10-10 17:22:04 +0200

    All tests green

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -1,10 +1,16 @@
 import { TestBed } from '@angular/core/testing';
 import { App } from './app';
+import { provideRouter } from '@angular/router';
+import { routes } from './app.routes';
+import { MockComponent } from 'ng-mocks';
+import { HomePage } from './home/home-page/home-page';
+import { ProductsPage } from './products/products-page/products-page';
 
 describe('App', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [App],
+      imports: [App, MockComponent(HomePage), MockComponent(ProductsPage)],
+      providers: [provideRouter(routes)]
     }).compileComponents();
   });
 
--- src/app/products/products-service.spec.ts
+++ src/app/products/products-service.spec.ts
@@ -1,12 +1,19 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ProductsService } from './products-service';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
 
 describe('ProductsService', () => {
   let service: ProductsService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        provideHttpClient(),
+        provideHttpClientTesting(),
+      ]
+    });
     service = TestBed.inject(ProductsService);
   });
 
